d5bdc529b321f362292d0d20b9f81e45db761691
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-21 00:15:42 +0530

    part-2

--- src/Components/Users/AddUsers.js
+++ src/Components/Users/AddUsers.js
@@ -13,6 +13,9 @@ const AddUsers = (props) => {
     if(enteredUsername.trim().length===0 || enteredAge.trim().length===0){
         return;
     }
+    if(enteredAge<1){
+        return
+    }
     console.log(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
