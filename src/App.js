cd1c8ce607a5770ab58be7d6a6e1d1cb7a67b2ff
HimanshuDeveloperr <himanshugupta241299@gmail.com>
2023-01-21 12:37:17 +0530

    AddedUserLists really imp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AddUsers from './Components/Users/AddUsers';
+import UserList from './Components/Users/UserList';
 function App() {
   return (
     <div>
       <AddUsers/>
+      <UserList users={[]}/>
     </div>
   );
 }
